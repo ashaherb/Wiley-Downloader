ee23b9031df00e6962226a4601c844801021933c
Smoodie <89829273+Smoodie7@users.noreply.github.com>
2023-05-14 13:17:16 +0100

    Update scraper.py
    -urllib and urllib2 have been replaced by the requests library, which is more modern and easier to use.
    -BeautifulSoup4 and lxml are still used for HTML parsing.
    -The PyPDF2 library is now used instead of pyPdf.
    -The deprecated file function has been replaced with open.
    -Exception handling has been improved for better error tracking.
    -Added more comments
    -Optimized the code

--- scraper.py
+++ scraper.py
@@ -1,65 +1,83 @@
 import os
-import urllib
-import urllib2
+import requests
 import sys
-from bs4 import BeautifulSoup as BS
-from pyPdf import PdfFileWriter, PdfFileReader
+from bs4 import BeautifulSoup
+from PyPDF2 import PdfFileWriter, PdfFileReader
 
-def append_pdf(input,output):
-	[output.addPage(input.getPage(page_num)) for page_num in range(input.numPages)]
+# Function to append pages from one PDF into another
+def append_pdf(input_pdf, output_pdf):
+	for page_num in range(input_pdf.getNumPages()):
+		output_pdf.addPage(input_pdf.getPage(page_num))
 
+# Base URL for Wiley's online library
 baseurl = "http://onlinelibrary.wiley.com"
 
+# Get the URL from command line argument
 url = sys.argv[1]
 
+# Check if the provided URL is valid
 if url.startswith("http"):
+	# Initialize PDF writer for the output
 	output = PdfFileWriter()
-	f = urllib.urlopen(url)
-	myfile = f.read()
-	parsed = BS(myfile,"lxml")
+
+	# Send a GET request to the provided URL
+	response = requests.get(url)
+
+	# Parse the returned HTML using BeautifulSoup
+	parsed = BeautifulSoup(response.content, "lxml")
+
+	# Find all citation elements and the product title in the parsed HTML
 	toc = parsed.find_all(class_="citation")
 	title = parsed.find(id="productTitle")
 	title_text = ''.join(title.findAll(text=True))
-	print("You are downloading " + title_text)
+
+	print(f"You are downloading {title_text}")
+
+	# For each citation element, find the standard PDF link
 	for counter, item in enumerate(toc):
 		pdf = item.find(class_="standardPdfLink")
 		pdflink = pdf.get('href')
 
+		# If the link is relative, append the base URL
 		if not pdflink.startswith("http"):
 			pdflink = baseurl + pdflink
 
-		try:
-			response = urllib2.urlopen(pdflink)
-			response_parsed = BS(response,"lxml")
-			pdftl = response_parsed.find(id="pdfDocument")
-			pdftlink = pdftl.get('src')
-		except:
-			print("Failed to get resource , Retrying (0/10)")
-			flag = 0
-			for i in range(10):
-				response = urllib2.urlopen(pdflink)
-				response_parsed = BS(response,"lxml")
+		# Retry up to 10 times to download and process the PDF
+		for attempt in range(10):
+			try:
+				# Get the page with the actual PDF link
+				response = requests.get(pdflink)
+
+				# Parse the returned HTML and find the PDF document element
+				response_parsed = BeautifulSoup(response.content, "lxml")
 				pdftl = response_parsed.find(id="pdfDocument")
-				if pdftl is not None:
-					flag = 1
-					break
-				print("Failed to get resource , Retrying ("+str(i)+"/10)")
-			if flag == 0:
-				print ("Couldn't Connect to get resources. Exiting")
-				exit()
-		pdftlink = pdftl.get('src')
-		pdffile = urllib2.urlopen(pdftlink)
+				pdftlink = pdftl.get('src')
 
-		file_seg = open(title_text + str(counter)+".pdf", 'wb')
-		file_seg.write(pdffile.read())
-		file_seg.close()
+				# Download the PDF
+				pdffile = requests.get(pdftlink)
 
-		append_pdf(PdfFileReader(file(title_text + str(counter)+".pdf","rb")),output)
-		os.remove(title_text + str(counter)+".pdf")
+				# Save the downloaded PDF to a temporary file
+				with open(f"{title_text}{counter}.pdf", 'wb') as file_seg:
+					file_seg.write(pdffile.content)
 
-		print ("Finished Processing : " + pdflink)
+				# Append the temporary PDF to the output PDF
+				append_pdf(PdfFileReader(open(f"{title_text}{counter}.pdf", "rb")), output)
 
-	output.write(file(title_text+ ".pdf","wb"))
+				# Delete the temporary PDF
+				os.remove(f"{title_text}{counter}.pdf")
+
+				print(f"Finished Processing : {pdftlink}")
+
+				# Successfully downloaded and processed PDF, so break from retry loop
+				break
+			except Exception as e:
+				print(f"Failed to get resource on attempt {attempt + 1}/10 due to error: {e}")
+				if attempt == 9:
+					print("Couldn't Connect to get resources. Exiting")
+					sys.exit(1)
+
+	# Save the combined PDF to a file
+	with open(f"{title_text}.pdf", "wb") as combined_pdf:
+		output.write(combined_pdf)
 else:
-	print ("This is not a valid URL")
-
+	print("This is not a valid URL")
